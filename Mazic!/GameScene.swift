a7e73b9a88ea58e01e7f4d9bb5dd04efd4bca74f
NandaMochammad <nanda.mocha@gmail.com>
2019-05-17 20:46:30 +0700

    Layer Mask DOne!

--- Mazic!/GameScene.swift
+++ Mazic!/GameScene.swift
@@ -16,6 +16,10 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     var player = SKSpriteNode()
     var goals = SKSpriteNode()
     
+    var locX : CGFloat?
+    var locY : CGFloat?
+//    var playerX :
+    
     override func didMove(to view: SKView) {
         
         self.physicsWorld.contactDelegate = self
@@ -24,22 +28,70 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         goals = self.childNode(withName: "Goals") as! SKSpriteNode
         
         manager.startAccelerometerUpdates()
-        manager.accelerometerUpdateInterval = 0.1
+        manager.accelerometerUpdateInterval = 0.5
         manager.startAccelerometerUpdates(to: OperationQueue.main){
             (data, error) in
-            self.physicsWorld.gravity = CGVector(dx: CGFloat((data?.acceleration.x)!) * 10, dy: CGFloat((data?.acceleration.y)!) * 10)
+            self.locX = CGFloat((data?.acceleration.x)!) * 10
+            self.locY = CGFloat((data?.acceleration.y)!) * 10
+            self.physicsWorld.gravity = CGVector(dx: self.locX!, dy: self.locY!)
+//            print("Player Location ", self.player.centerRect.maxX, self.player.centerRect.maxY)
+
         }
+        
+        self.addOverlay()
+
+        
         
     }
     
     func didBegin(_ contact: SKPhysicsContact) {
-        var bodyA = contact.bodyA
-        var bodyB = contact.bodyB
+        let bodyA = contact.bodyA
+        let bodyB = contact.bodyB
         
         if bodyA.categoryBitMask == 1 && bodyB.categoryBitMask == 2 || bodyA.categoryBitMask == 2 && bodyB.categoryBitMask == 1{
             print("Goals")
             
         }
+    }
+    
+    func addOverlay() {
+        let randA = CGFloat.random(in: 0 ... 350)
+        let randB = CGFloat.random(in: 0 ... 800)
+        let overlay = createOverlay(frame: view!.frame,
+                                    xOffset: randA,
+                                    yOffset: randB,
+                                    radius: 50.0)
+        view!.addSubview(overlay)
+    }
+    
+    func createOverlay(frame: CGRect,
+                       xOffset: CGFloat,
+                       yOffset: CGFloat,
+                       radius: CGFloat) -> UIView {
+        print(xOffset, yOffset)
+        print(frame)
+        // Step 1
+        let overlayView = UIView(frame: frame)
+        overlayView.backgroundColor = UIColor.black.withAlphaComponent(0.6)
+        // Step 2
+        let path = CGMutablePath()
+        path.addArc(center: CGPoint(x: xOffset, y: yOffset),
+                    radius: radius,
+                    startAngle: 0.0,
+                    endAngle: 2.0 * .pi,
+                    clockwise: false)
+        path.addRect(CGRect(origin: .zero, size: overlayView.frame.size))
+        // Step 3
+        let maskLayer = CAShapeLayer()
+        maskLayer.backgroundColor = UIColor.black.withAlphaComponent(0.2).cgColor
+        maskLayer.path = path
+        // For Swift 4.2
+        maskLayer.fillRule = .evenOdd
+        // Step 4
+        overlayView.layer.mask = maskLayer
+        overlayView.clipsToBounds = true
+        
+        return overlayView
     }
     
     override func update(_ currentTime: TimeInterval) {
