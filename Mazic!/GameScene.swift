bc847d807a1bfbed92014926cbfd72e43cb03db8
NandaMochammad <nanda.mocha@gmail.com>
2019-05-17 19:14:28 +0700

    Ad Goal

--- Mazic!/GameScene.swift
+++ Mazic!/GameScene.swift
@@ -14,13 +14,14 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     let manager = CMMotionManager()
     var player = SKSpriteNode()
-    var endNode = SKSpriteNode()
+    var goals = SKSpriteNode()
     
     override func didMove(to view: SKView) {
         
         self.physicsWorld.contactDelegate = self
         
         player = self.childNode(withName: "player") as! SKSpriteNode
+        goals = self.childNode(withName: "Goals") as! SKSpriteNode
         
         manager.startAccelerometerUpdates()
         manager.accelerometerUpdateInterval = 0.1
@@ -29,6 +30,16 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             self.physicsWorld.gravity = CGVector(dx: CGFloat((data?.acceleration.x)!) * 10, dy: CGFloat((data?.acceleration.y)!) * 10)
         }
         
+    }
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        var bodyA = contact.bodyA
+        var bodyB = contact.bodyB
+        
+        if bodyA.categoryBitMask == 1 && bodyB.categoryBitMask == 2 || bodyA.categoryBitMask == 2 && bodyB.categoryBitMask == 1{
+            print("Goals")
+            
+        }
     }
     
     override func update(_ currentTime: TimeInterval) {
