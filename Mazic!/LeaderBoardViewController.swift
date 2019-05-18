f008e22c78120205963a8a6bd3b434e75b4b968a
NandaMochammad <nanda.mocha@gmail.com>
2019-05-18 14:28:49 +0700

    Add New Page for Home + Leader Board

---
+++ Mazic!/LeaderBoardViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LeaderBoardViewController.swift
+//  Mazic!
+//
+//  Created by Nanda Mochammad on 18/05/19.
+//  Copyright © 2019 nandamochammad. All rights reserved.
+//
+
+import UIKit
+
+class LeaderBoardViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
